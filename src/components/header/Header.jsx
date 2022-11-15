0f29d6d1bf78b0a6a8c59c8e38852c953fa3d1ba
Kasidet watthanaphonphairot <kasidetwatthanaphonphairot@MacBook-Air-khxng-Kasidet.local>
2022-11-15 20:37:35 +0700

    Add existing file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.css'
 
 const Header = () => {
+    /* ===============Toggle Menu ===============*/ 
+    const[Toggle, ShowMenu] = useState(false);
   return (
     <header className="header">
         <nav className="nav container">
-            <a href="index.html" className="nav__logo">Smith</a>
-            <div className="nav__menu">
+            <a href="index.html" className="nav__logo">
+                Smith
+            </a>
+            <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                 <ul className="nav__list grid">
 
                     <li className="nav__item">
@@ -47,9 +51,9 @@ const Header = () => {
 
                 </ul>
 
-                <i class="uil uil-times nav__close"></i>
+                <i class="uil uil-times nav__close" onClick={() => ShowMenu(!Toggle)}></i>
             </div>
-            <div className="nav__toggle">
+            <div className="nav__toggle" onClick={() => ShowMenu(!Toggle)}>
                 <i class="uil uil-apps"></i>
             </div>
         </nav>
